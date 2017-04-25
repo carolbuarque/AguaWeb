06a56560087c52e98bbf3b0df2e2d70a1b7e4431
acsbuarque <acsbuarque@gmail.com>
2017-04-25 16:58:36 -0300

    Altera banco de dados para POSTGRESQL

--- aguaweb/settings.py
+++ aguaweb/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'aguaweb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'aguaweb',
+        'USER': 'postgres',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
